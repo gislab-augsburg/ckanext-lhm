9f54a80a129db0ca899c17706171fdfc848db56b
mandana.moshrefzadeh <mandana.moshrefzadeh@muenchen.de>
2024-03-08 15:51:43 +0100

    version 1.1.3 für Tester

--- ckanext/lhm/plugin.py
+++ ckanext/lhm/plugin.py
@@ -12,7 +12,6 @@ from ckanext.lhm.logic import action
 #     (action, auth, validators
 # )
 
-g = p.toolkit.g
 
 #from ckanext.datastore.backend.postgres import _cache_types
 from sqlalchemy import create_engine
@@ -115,79 +114,6 @@ class LHMCatalogPlugin(p.SingletonPlugin, DefaultTranslation):
         #     'user_info',#: helpers.user_info
         # ])
 
-    def before_dataset_search(self, search_params):
-        '''When searching an organization or group, optionally extend the search any
-        sub-organizations or sub-groups too. This is achieved by modifying the search options
-        before they go to SOLR.
-        '''
-        # Check if we're called from the organization controller, as detected
-        # by g being registered for this thread, and the existence of g.fields
-        # values
-
-        try:
-            if not isinstance(g.fields, list) and not hasattr(g, 'fields'):
-                return search_params
-        except (TypeError, AttributeError, RuntimeError):
-            # it's non-organization controller or CLI call
-            return search_params
-
-        if toolkit.check_ckan_version("2.10"):
-            controller = toolkit.get_endpoint()[0]
-        else:
-            controller = g.controller
-
-        # e.g. search_params['q'] = u' owner_org:"id" include_children: "True"'
-        query = search_params.get('q', '')
-        fq = search_params.get('fq', '')
-
-        # Always set include_children to True by default
-        include_children = True
-
-        if include_children or helpers.is_include_children_selected():
-
-            # get a list of all the children organizations or groups and include them in
-            # the search params
-            children_hierarchy = model.Group.get(g.group_dict.get('id')).\
-                get_children_group_hierarchy(type=controller)
-            children_names = [grp[1] for grp in children_hierarchy]
-            # remove include_children clause - it is a message for this func,
-            # not solr
-            # CKAN<=2.7 it's in the q field:
-            query = query.replace('include_children: "True"', '')
-            # CKAN=2.8.x it's in the fq field:
-            fq = fq.replace('include_children:"True"', '')
-
-            if children_names:
-                # remove existing owner_org:"<parent>" or groups:"<parent>" clause - we'll replace
-                # it with the tree of orgs or groups in a moment.
-                if controller == "organization":
-                    group_q = 'owner_org:"{}"'.format(g.group_dict.get('id'))
-                    query_prefix = 'organization'
-                elif controller == "group":
-                    group_q = 'groups:"{}"'.format(g.group_dict.get('name'))
-                    query_prefix = 'groups'
-                # CKAN<=2.7 it's in the q field:
-                query = query.replace(group_q, '')
-                # CKAN=2.8.x it's in the fq field:
-                fq = fq.replace(group_q, '')
-                # add the org or grp clause
-                query = query.strip()
-                if query:
-                    query += ' AND '
-                query += '({})'.format(
-                    ' OR '.join(
-                        query_prefix+':{}'.format(org_name)
-                        for org_name in [g.group_dict.get('name')] +
-                        children_names))
-
-            search_params['q'] = query.strip()
-            search_params['fq'] = fq
-            # remove include_children from the filter-list - we have a checkbox
-            g.fields_grouped.pop('include_children', None)
-
-        return search_params
-
-    before_search = before_dataset_search
 
     def before_dataset_index(self, data_dict):      
         return self.before_index(data_dict)

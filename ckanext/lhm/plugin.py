e0c5cb2ebc8df04f8e8f8be343da73aab6d8dbd6
Martin Bayr <ex.bayr@muenchen.de>
2026-01-28 16:44:46 +0100

    delete theme_templates and assets_theme, move theme_templates_2.9.9 and assets_theme_2.9.9 to theme_templates and assets_theme, adapt plugin.py for new structure

--- ckanext/lhm/plugin.py
+++ ckanext/lhm/plugin.py
@@ -248,20 +248,13 @@ class LHMThemePlugin(p.SingletonPlugin, DefaultTranslation):
     p.implements(p.IFacets, inherit=True)
     p.implements(p.IActions)
     #p.implements(p.ITemplateHelpers)
+    
 
-    if toolkit.check_ckan_version("2.9"):
-        # IConfigurer
-        def update_config(self, config):
-            p.toolkit.add_template_directory(config, 'theme_templates_2.9.9')
-            p.toolkit.add_public_directory(config, 'public')
-            p.toolkit.add_resource('assets_theme_2.9.9', 'lhm_theme')
-
-    elif toolkit.check_ckan_version("2.10"):
-        # IConfigurer
-        def update_config(self, config):
-            p.toolkit.add_template_directory(config, 'theme_templates')
-            p.toolkit.add_public_directory(config, 'public')
-            p.toolkit.add_resource('assets_theme', 'lhm_theme')
+    # IConfigurer
+    def update_config(self, config):
+        p.toolkit.add_template_directory(config, 'theme_templates')
+        p.toolkit.add_public_directory(config, 'public')
+        p.toolkit.add_resource('assets_theme', 'lhm_theme')
 
 
     # IActions

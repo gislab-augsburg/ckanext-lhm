cd600565e39eed9a95e7317536c9f4047bf1c980
saskia.landgraf <saskia.landgraf@muenchen.de>
2026-08-26 10:42:22 +0200

    variablen umbenennung und registrierung des properties

--- ckanext/lhm/views.py
+++ ckanext/lhm/views.py
@@ -18,10 +18,10 @@ import requests
 lhm_view = Blueprint('lhm_view', __name__)
 
 def get_pycsw_host_url():
-    host = toolkit.config.get('ckanext.lhm.pycsw_host')
+    host = toolkit.config.get('lhm.pycsw_url')
     
     if not host or not host.strip():
-        host = os.environ.get('CKANEXT__LHM__PYCSW_HOST')
+        host = os.environ.get('CKANEXT__LHM__PYCSW_URL')
         
     if not host or not host.strip():
         host = 'http://pycsw:8000'
